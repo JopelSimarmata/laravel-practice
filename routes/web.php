b49b219fc15a3b40390fe24c73dda23407b3a91f
Jopel Simarmata <iss23036@students.del.ac.id>
2025-08-30 23:50:48 +0700

    add class post

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,28 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+class Post
+{
+    public static function all()
+    {
+        return [        [
+            "id" => 1,
+            "title" => "Judul Artikel 1",
+            "slug" => "judul-artikel-1",
+            "author" => "Jopel Simarmata",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores, nihil! Debitis dolore incidunt iure? Architecto quaerat alias iste quo recusandae praesentium laborum, dolor harum! Veritatis ut repellendus assumenda dolorum reprehenderit!"
+        ],
+        [
+            "id" => 2,
+            "title" => "Judul Artikel 2",
+            "slug" => "judul-artikel-2",
+            "author" => "Jopel Simarmata",
+            //buat body nya berbeda dari diatas
+            "body" => "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
+        ]];
+    }
+}
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
@@ -20,23 +42,7 @@ Route::get('/home', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog Page', 'posts' => [
-        [
-            "id" => 1,
-            "title" => "Judul Artikel 1",
-            "slug" => "judul-artikel-1",
-            "author" => "Jopel Simarmata",
-            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores, nihil! Debitis dolore incidunt iure? Architecto quaerat alias iste quo recusandae praesentium laborum, dolor harum! Veritatis ut repellendus assumenda dolorum reprehenderit!"
-        ],
-        [
-            "id" => 2,
-            "title" => "Judul Artikel 2",
-            "slug" => "judul-artikel-2",
-            "author" => "Jopel Simarmata",
-            //buat body nya berbeda dari diatas
-            "body" => "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-        ]
-    ]]);
+    return view('posts', ['title' => 'Blog Page', 'posts' => Post::all()]);
 });
 
 Route::get('/contact', function () {
@@ -44,23 +50,7 @@ Route::get('/contact', function () {
 });
 
 Route::get('/posts/{slug}', function($slug){
-    $posts = [
-        [
-            "id" => 1,
-            "title" => "Judul Artikel 1",
-            "slug" => "judul-artikel-1",
-            "author" => "Jopel Simarmata",
-            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores, nihil! Debitis dolore incidunt iure? Architecto quaerat alias iste quo recusandae praesentium laborum, dolor harum! Veritatis ut repellendus assumenda dolorum reprehenderit!"
-        ],
-        [
-            "id" => 2,
-            "title" => "Judul Artikel 2",
-            "slug" => "judul-artikel-2",
-            "author" => "Jopel Simarmata",
-            //buat body nya berbeda dari diatas
-            "body" => "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-        ]
-    ];
+    $posts = Post::all();
 
     $post = Arr::first($posts, function($post) use ($slug){
         return $post['slug'] == $slug;
