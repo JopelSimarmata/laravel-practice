a114680e1bab6398d35e755746ff7fa10ad84c23
Jopel Simarmata <iss23036@students.del.ac.id>
2025-09-06 15:15:29 +0700

    add factory

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Post extends Model //terhubung ke tabel yang namanya posts
+class Post extends Model
 {
+    use HasFactory;
     protected $fillable = ['title', 'author', 'slug', 'body'];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
