60e42e556b749d154c4e945596ad84f8e82f0f7a
Jopel Simarmata <iss23036@students.del.ac.id>
2025-09-06 23:16:43 +0700

    fix view category model

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Arr;
@@ -43,6 +44,15 @@ Route::get('/authors/{user}', function(User $user){
     
     return view('posts', ['title' => 'Articles by '.$user->name, 'posts' => $user->posts]);
 });
+
+Route::get('/category/{category}', function(Category $category){
+   
+    //arr first digunakan untuk mengambil elemen pertama yang sesuai kondisi
+    
+    return view('posts', ['title' => 'Category : '.$category->category, 'posts' => $category->posts]);
+});
+
+
 
 
 
